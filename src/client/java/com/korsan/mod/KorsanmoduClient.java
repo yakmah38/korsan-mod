16391e0210475e12f64c1d596c2d59e58a188055
yakmah38 <yakmah38@gmail.com>
2026-01-15 18:34:40 +0300

    tam dosya eklenme işlemi

---
+++ src/client/java/com/korsan/mod/KorsanmoduClient.java
@@ -0,0 +1,10 @@
+package com.korsan.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KorsanmoduClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
